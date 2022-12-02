285c1cbb434e13d390f21e246b6b037bb1bbf9a8
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-12-02 21:02:43 +0300

    Add files via upload

---
+++ notes/notes.js
@@ -0,0 +1,62 @@
+"use strict";
+/*
+//array declaration
+const arr1 = new Array(1, 'sdfa', 34);
+const arr2 = ['aSD', 123, arr1];
+
+arr1.push(54);
+arr1.unshift(13); //inserts element to the first index
+
+arr1.pop();
+arr1.shift();   //deletes first element
+
+
+
+
+
+//objects = dictionary
+const jonas = {
+    firstName: 'Jonas',
+    lastNme: 'Obama',
+    friends: ['Micheal', 'Peter', 'Steven'],
+    calcAge: function (birthDate) {
+        this.age = 2022 - birthDate;
+        return this.age;
+    },
+    driverLicence: true
+}
+console.log(jonas[1]);
+//accessing elements
+jonas['firstName'];
+//or
+jonas.firstName
+let age = jonas.calcAge(2002);
+let age2 = jonas['calcAge'](2002)
+
+
+const total = function (obj) {
+    console.log(`${obj.firstName} is ${obj.calcAge(2002)} years old and ${(obj.driverLicence) ? 'has' : "doesnt has"} a driver license.`);
+}
+total(jonas);
+
+
+
+
+
+const randomNum = Math.trunc(Math.random() * 5); //this wil return a random number between 1 and 5
+
+console.log(6);
+console.log('6');
+
+
+
+
+//downloading live-server package on terminal:
+node -v     //checks the version
+
+npm install live-server -g   //to install live-server
+
+live-server  //to run it on browser
+
+
+*/
