ed7aabdc1280dd93dc4ae6b0e744dd1d79212808
Mehmet <75950443+PloKoon68@users.noreply.github.com>
2022-12-04 14:11:40 +0300

    Update script.js

--- modal/script.js
+++ modal/script.js
@@ -8,18 +8,22 @@ const btnCloseModal = document.querySelector('.close-modal');
 //a node list
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-//when looping trough this list
-for (let i = 0; i < btnOpenModal.length; i++)
-  //when clicking any of the buttons, the modal will show up
-  btnOpenModal[i].addEventListener('click', function () {
+
+const openModal =  function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-  });
+  };
 
 const closeModal = function() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 }
+
+//when looping trough this list
+for (let i = 0; i < btnOpenModal.length; i++)
+  //when clicking any of the buttons, the modal will show up
+  btnOpenModal[i].addEventListener('click', openModal);
+
 
 //after opening the modal, close it when clicking the close button
 btnCloseModal.addEventListener("click", closeModal);
